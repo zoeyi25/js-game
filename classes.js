acd32e27e86a0298715cdbaedc7049c53042a8d2
Zoe Yi <zoe.yi.25@bishops.com>
2022-12-23 22:25:45 -0800

    added move function

--- classes.js
+++ classes.js
@@ -1,7 +1,7 @@
 // function to create rectangle objects
 class Rectangle {
   // arguments passed in
-  constructor(x, y, width, height, fillColor, strokeColor, strokeWidth) {
+  constructor(x, y, width, height, fillColor, strokeColor, strokeWidth, canvasHeight) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -9,6 +9,9 @@ class Rectangle {
     this.fillColor = fillColor;
     this.strokeColor = strokeColor;
     this.strokeWidth = strokeWidth;
+    this.canvasHeight = canvasHeight;
+    this.direction = "up";
+    this.move();
   }
 
   get pos()
@@ -17,22 +20,17 @@ class Rectangle {
   }
 
   draw() {
-    // destructuring
-    const {
-      x, y, width, height, fillColor, strokeColor, strokeWidth
-    } = this;
-
     // save the current styles set elsewhere to avoid overwriting them
     ctx.save();
 
     // set the styles for this shape
-    ctx.fillStyle = fillColor;
-    ctx.lineWidth = strokeWidth;
+    ctx.fillStyle = this.fillColor;
+    ctx.lineWidth = this.strokeWidth;
 
     // create path
     ctx.beginPath();
-    ctx.strokeStyle = strokeColor;
-    ctx.rect(x, y, width, height);
+    ctx.strokeStyle = this.strokeColor;
+    ctx.rect(this.x, this.y, this.width, this.height);
 
     // draw the path to screen
     ctx.fill();
@@ -41,5 +39,33 @@ class Rectangle {
     // restore the styles from earlier preventing the colors used here from
     // polluting other drawings
     ctx.restore();
+  }
+
+  move()
+  {
+    console.log("moved")
+    const speed = 1;
+    const id = setInterval(() =>
+    {
+      console.log("y = " + this.y)
+      // code between these curly brackets {} will run repeatedly at interval
+
+      ctx.clearRect(this.x - 1, this.y - 1, this.width + 2, this.height + 2);
+
+      if (this.y <= this.canvasHeight * 0.1) {
+        this.direction = "down";
+      }
+      if (this.y >= this.canvasHeight * 0.9) {
+        this.direction = "up";
+      }
+
+      if (this.direction == "up"){
+          this.y -= speed;
+      }
+      else {
+          this.y += speed;
+      }
+      this.draw();
+    }, 50);   //repeat interval every 50 ms
   }
 }

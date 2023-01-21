22bfc9fe0e5c9ddd6afb652ee778bd7b7bb87dd4
Zoe Yi <zoe.yi.25@bishops.com>
2023-01-20 23:48:05 -0800

    added images, rotation, fruit splitting, start/end screen

--- classes.js
+++ classes.js
@@ -1,7 +1,6 @@
-// function to create rectangle objects
 class Rectangle {
-  // arguments passed in
-  constructor(x, y, width, height, fillColor, strokeColor, strokeWidth, canvasHeight) {
+  constructor(x, y, width, height, fillColor, strokeColor, strokeWidth) {
+    this.fruit = "";
     this.x = x;
     this.y = y;
     this.width = width;
@@ -9,10 +8,50 @@ class Rectangle {
     this.fillColor = fillColor;
     this.strokeColor = strokeColor;
     this.strokeWidth = strokeWidth;
-    this.canvasHeight = canvasHeight;
     this.direction = "up";
-    this.move();
     this.id;
+    this.deg = 0;
+
+    melon = new Image();
+    melon.src = "assets/melon.png";
+    melonSlice1 = new Image();
+    melonSlice1.src = "assets/melonSlice1.png";
+    melonSlice2 = new Image();
+    melonSlice2.src = "assets/melonSlice2.png";
+    apple = new Image();
+    apple.src = "assets/apple.png";
+    appleSlice1 = new Image();
+    appleSlice1.src = "assets/appleSlice1.png";
+    appleSlice2 = new Image();
+    appleSlice2.src = "assets/appleSlice2.png";
+    berry = new Image();
+    berry.src = "assets/berry.png";
+    berrySlice1 = new Image();
+    berrySlice1.src = "assets/berrySlice1.png";
+    berrySlice2 = new Image();
+    berrySlice2.src = "assets/berrySlice2.png";
+    mango = new Image();
+    mango.src = "assets/mango.png";
+    mangoSlice1 = new Image();
+    mangoSlice1.src = "assets/mangoSlice1.png";
+    mangoSlice2 = new Image();
+    mangoSlice2.src = "assets/mangoSlice2.png";
+    bomb = new Image();
+    bomb.src = "assets/bomb.png";
+
+    this.data = {
+      "melon": {"xOffset": 16, "yOffset": 14, "width": 98, "height": 85, "whole": melon, "slice1": melonSlice1, "slice2": melonSlice2},
+      "apple": {"xOffset": 10, "yOffset": 10, "width": 66, "height": 66, "whole": apple, "slice1": appleSlice1, "slice2": appleSlice2},
+      "berry": {"xOffset": 8, "yOffset": 18, "width": 68, "height": 72, "whole": berry, "slice1": berrySlice1, "slice2": berrySlice2},
+      "mango": {"xOffset": 10, "yOffset": 10, "width": 62, "height": 59, "whole": mango, "slice1": mangoSlice1, "slice2": mangoSlice2},
+      "bomb": {"xOffset": 16, "yOffset": 22, "width": 66, "height": 68, "whole": bomb}
+    };
+    this.move();
+  }
+
+  setFruit(fruit)
+  {
+    this.fruit = fruit;
   }
 
   get pos()
@@ -24,20 +63,8 @@ class Rectangle {
     return this.direction;
   }
 
-  getFillColor() {
-    return this.fillColor;
-  }
-
   isGameOver() {
     return this.gameOver;
-  }
-
-  setWidth(value) {
-    this.width = value;
-  }
-
-  setHeight(value) {
-    this.height = value;
   }
 
   draw() {
@@ -48,12 +75,10 @@ class Rectangle {
     ctx.fillStyle = this.fillColor;
     ctx.lineWidth = this.strokeWidth;
 
-    // create path
     ctx.beginPath();
     ctx.strokeStyle = this.strokeColor;
     ctx.rect(this.x, this.y, this.width, this.height);
 
-    // draw the path to screen
     ctx.fill();
     ctx.stroke();
 
@@ -62,27 +87,88 @@ class Rectangle {
     ctx.restore();
   }
 
-  move() {
-    const a = 10;
-    const timeInterval = 0.5;
-    let vY = -115;
-    const vX = this.x <= c.width * 0.5? 20 : -20;
-    this.id = setInterval(() =>
-    {
-      // code between these curly brackets {} will run repeatedly at interval
-      ctx.clearRect(this.x - 1, this.y - 1, this.width + 2, this.height + 2);
+move() {
+  const acceleration = 11;
+  const timeInterval = 0.4;
+  let vY = -115;
+  const vX = this.x <= c.width * 0.5? 20 : -20;
+  this.id = setInterval(() =>
+  {
+    // rotate canvas to last rotated position to clear fruit/bomb
+    ctx.save();
+    ctx.translate(this.x + (this.width / 2), this.y + (this.height / 2));
+    ctx.rotate(this.deg);
+    ctx.translate(-(this.x + (this.width / 2)), -(this.y + (this.height / 2)));
+    ctx.clearRect(this.x - this.data[this.fruit]["xOffset"], this.y - this.data[this.fruit]["yOffset"], this.data[this.fruit]["width"] + 2, this.data[this.fruit]["height"] + 2);
+    ctx.restore();
 
-      if (vY >= 0) {
-        this.direction = "down";
-      }
+    this.deg += 0.1;
 
-      vY += a * timeInterval;
-      this.y += vY * timeInterval;
-      this.x += vX * timeInterval;
+    if (vY >= 0) {
+      this.direction = "down";
+    }
 
-      this.draw();
-    }, 50);   //repeat interval every 50 ms
-  }
+    vY += acceleration * timeInterval;
+    this.y += vY * timeInterval;
+    this.x += vX * timeInterval;
+
+    this.draw();
+    if (this.fruit == "bomb") {
+      ctx.save();
+      ctx.translate(this.x + (this.width / 2), this.y + (this.height / 2));
+      ctx.rotate(this.deg);
+      ctx.translate(-(this.x + (this.width / 2)), -(this.y + (this.height / 2)));
+      ctx.drawImage(bomb, this.x - this.data["bomb"]["xOffset"], this.y - this.data["bomb"]["yOffset"]);
+      ctx.restore();
+    }
+    else {
+      ctx.save();
+      ctx.translate(this.x + (this.width / 2), this.y + (this.height / 2));
+      ctx.rotate(this.deg);
+      ctx.translate(-(this.x + (this.width / 2)), -(this.y + (this.height / 2)));
+      ctx.drawImage(this.data[this.fruit]["whole"], this.x - this.data[this.fruit]["xOffset"], this.y - this.data[this.fruit]["yOffset"]);
+      ctx.restore();
+    }
+  }, 50);
+}
+
+fall() {
+  const acceleration = 10;
+  const timeInterval = 0.5;
+  let vY = 0;
+  const splitOffset = 30;
+  this.id = setInterval(() =>
+  {
+    // clear
+    ctx.save();
+    ctx.translate(this.x + (this.width / 2), this.y + (this.height / 2));
+    ctx.rotate(this.deg);
+    ctx.translate(-(this.x + (this.width / 2)), -(this.y + (this.height / 2)));
+    ctx.clearRect(this.x - splitOffset, this.y, this.data[this.fruit]["width"], this.data[this.fruit]["height"]);
+    ctx.clearRect(this.x + splitOffset, this.y, this.data[this.fruit]["width"], this.data[this.fruit]["height"]);
+    ctx.restore();
+
+    // increment
+    this.deg += 0.1;
+    vY += acceleration * timeInterval;
+    this.y += vY * timeInterval;
+
+    // draw
+    ctx.save();
+    ctx.translate(this.x + (this.width / 2), this.y + (this.height / 2));
+    ctx.rotate(this.deg);
+    ctx.translate(-(this.x + (this.width / 2)), -(this.y + (this.height / 2)));
+    if (this.fruit != "bomb"){
+      ctx.drawImage(this.data[this.fruit]["slice1"], this.x - splitOffset, this.y);
+      ctx.drawImage(this.data[this.fruit]["slice2"], this.x + splitOffset, this.y);
+    }
+    ctx.restore();
+
+    if (this.y >= c.height + 100) {
+      clearInterval(this.id);
+    }
+  }, 50);
+}
 
 stopMoving() {
   clearInterval(this.id);
--- game.js
+++ game.js
@@ -1,28 +1,49 @@
+// references:
+// https://stackoverflow.com/questions/29300280/update-html5-canvas-rectangle-on-hover
+// https://stackoverflow.com/questions/6011378/how-to-add-image-to-canvas
+// https://stackoverflow.com/questions/3793397/html5-canvas-drawimage-with-at-an-angle
+// and others but I don't remember which ones so I don't have the links
+
 const c = document.getElementById("canvas");
 const ctx = c.getContext("2d");
 let counter = 0;
 let chances = 3;
 let origTime = 0;
 let rectangles = [];
-let totalSingleRects = 0;
-let lastBirthday = 0;
-let timeSinceBirthday = 0;
 
-var intervalID = setInterval(checkGameOver, 50);
-var intervalID2 = setInterval(newRects, 4000);
-var intervalID3 = setInterval(createBomb, 70000);
-var intervals = [intervalID, intervalID2, intervalID3];
+var intervalCheckGameOverID;
+var intervalNewRectsID;
+var intervalCreateBombID;
+var intervals;
 
-function onMouseDown()
+function setUpAssets()
 {
-  const now = new Date();
-  origTime = now.getTime();
-}
-
-function onMouseUp()
-{
-  const now = new Date();
-  alert((now.getTime() - origTime) / 1000);
+  melon = new Image();
+  melon.src = "assets/melon.png";
+  melonSlice1 = new Image();
+  melonSlice1.src = "assets/melonSlice1.png";
+  melonSlice2 = new Image();
+  melonSlice2.src = "assets/melonSlice2.png";
+  apple = new Image();
+  apple.src = "assets/apple.png";
+  appleSlice1 = new Image();
+  appleSlice1.src = "assets/appleSlice1.png";
+  appleSlice2 = new Image();
+  appleSlice2.src = "assets/appleSlice2.png";
+  berry = new Image();
+  berry.src = "assets/berry.png";
+  berrySlice1 = new Image();
+  berrySlice1.src = "assets/berrySlice1.png";
+  berrySlice2 = new Image();
+  berrySlice2.src = "assets/berrySlice2.png";
+  mango = new Image();
+  mango.src = "assets/mango.png";
+  mangoSlice1 = new Image();
+  mangoSlice1.src = "assets/mangoSlice1.png";
+  mangoSlice2 = new Image();
+  mangoSlice2.src = "assets/mangoSlice2.png";
+  bomb = new Image();
+  bomb.src = "assets/bomb.png";
 }
 
 function randomColor()
@@ -38,24 +59,27 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function setUpCounter() {
+function setUpCounter()
+{
   ctx.font = "50px Arial";
-  ctx.fillStyle = "#000000"
-  ctx.fillText("0", c.width * 0.01, c.height * 0.07);
+  ctx.fillStyle = "#000000";
+  ctx.fillText(counter, c.width * 0.03, c.height * 0.07);
 }
 
-function setUpChances() {
-  ctx.fillStyle = "#06038D"
+function setUpChances()
+{
+  ctx.fillStyle = "#06038D";
   ctx.fillText("x", c.width * 0.85, c.height * 0.07);
   ctx.fillText("x", c.width * 0.9, c.height * 0.07);
   ctx.fillText("x", c.width * 0.95, c.height * 0.07);
 }
 
-function updateCounter() {
+function updateCounter()
+{
   counter += 1
-  ctx.clearRect(0, 0, 200, 100)
-  ctx.fillStyle = "#000000"
-  ctx.fillText(counter, c.width * 0.01, c.height * 0.07);
+  ctx.clearRect(0, 0, 200, 100);
+  ctx.fillStyle = "#000000";
+  ctx.fillText(counter, c.width * 0.03, c.height * 0.07);
 }
 
 function onMouseMove(e)
@@ -66,51 +90,62 @@ function onMouseMove(e)
   const mouseY = Math.round(e.clientY - cRect.top);
   for (let i in rectangles) {
     // get x, y, width, and height of rectangle
-    const x = rectangles[i].pos[0]
-    const y = rectangles[i].pos[1]
-    const width = rectangles[i].pos[2]
-    const height = rectangles[i].pos[3]
+    const x = rectangles[i].pos[0];
+    const y = rectangles[i].pos[1];
+    const width = rectangles[i].pos[2];
+    const height = rectangles[i].pos[3];
 
     // slice
     const now = new Date();
-    if (x <= mouseX && mouseX <= x + width && y <= mouseY && mouseY <= y + height && e.buttons == 1 && now.getTime() - origTime > 100) {
-      ctx.clearRect(x - 1, y - 1, width + 2, height + 2);
+    if (x <= mouseX && mouseX <= x + width && y <= mouseY && mouseY <= y + height) {
       // if bomb
-      if (width == 200 & height == 200) {
+      if (width == 35) {
         gameOver();
       }
       else {
-        rectangles[i].stopMoving();
-        delete rectangles[i];
         updateCounter();
+        rectangles[i].stopMoving();
+        rectangles[i].fall();
+        delete rectangles[i];
       }
     }
+    setUpCounter();
   }
 }
 
 function gameOver()
 {
-  console.log("game over");
   for (let i in rectangles) {
     rectangles[i].stopMoving();
   }
   for (let i in intervals) {
     clearInterval(intervals[i]);
   }
+
   ctx.clearRect(0, 0, c.width, c.height);
-  ctx.fillStyle = "#000000"
-  ctx.fillText("u suck", c.width * 0.5, c.height * 0.5);
+  ctx.fillStyle = "#000000";
+  ctx.fillText("game over", c.width * 0.5, c.height * 0.5);
   ctx.fillText("score: " + counter, c.width * 0.5, c.height * 0.6);
+  setTimeout(() => {
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.font = "50px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("new game", c.width * 0.5, c.height * 0.5);
+  }, 1000)
+  setTimeout(() => {
+    location.reload();
+  }, 2000);
 }
 
 function checkGameOver()
 {
   for (let i in rectangles) {
-    if (rectangles[i].pos[1] >= c.height & rectangles[i].getDirection() == "down") {
+    if (rectangles[i].pos[1] >= c.height && rectangles[i].getDirection() == "down" && rectangles[i].pos[2] != 35 && rectangles[i].pos[3] != 35) {
       chances -= 1;
       delete rectangles[i];
     }
   }
+  setUpChances();
   if (chances < 3) {
     ctx.fillStyle = "#FF0000";
   }
@@ -128,7 +163,32 @@ function checkGameOver()
 
 function makeOneRect()
 {
-  rectangles.push(new Rectangle(randomInt(1, c.width * 0.9), c.height, randomInt(50, 100), randomInt(50, 100), randomColor(), "#000000", 0, c.height));
+  let fruit = "";
+  let w = 0;
+  let h = 0;
+  const int = randomInt(0, 3);
+  if (int == 0) {
+    fruit = "apple";
+    w = 48;
+    h = 48;
+  }
+  else if (int == 1) {
+    fruit = "melon";
+    w = 56;
+    h = 56;
+  }
+  else if (int == 2) {
+    fruit = "berry";
+    w = 40;
+    h = 40;
+  }
+  else if (int == 3) {
+    fruit = "mango";
+    w = 46;
+    h = 38;
+  }
+  rectangles.push(new Rectangle(randomInt(1, c.width * 0.9), c.height, w, h, randomColor(), "#000000", 0));
+  rectangles[rectangles.length - 1].setFruit(fruit);
   rectangles[rectangles.length - 1].draw();
 }
 
@@ -141,35 +201,60 @@ function newRects()
     }
   }
   else {
-    setTimeout(() => {
-      makeOneRect();
-    }, 1000)
-    setTimeout(() => {
-      makeOneRect();
-    }, 2000)
-    setTimeout(() => {
-      makeOneRect();
-    }, 3000)
+    let timeouts = [];
+    const int = randomInt(1, 4);
+    if (int < 2) {
+      let timeoutID = setTimeout(makeOneRect(), 1000);
+      timeouts.push(timeoutID);
+    }
+    else if (int < 3) {
+      let timeoutID2 = setTimeout(makeOneRect(), 2000);
+      timeouts.push(timeoutID2);
+    }
+    else if (int < 4) {
+      let timeoutID3 = setTimeout(makeOneRect(), 3000);
+      timeouts.push(timeoutID3);
+    }
+    else if (int < 5) {
+      let timeoutID4 = setTimeout(makeOneRect(), 4000);
+      timeouts.push(timeoutID4);
+    }
   }
 }
 
 function createBomb()
 {
-  rectangles.push(new Rectangle(randomInt(1, c.width * 0.9), c.height, 200, 200, "#000000", "#000000", 0, c.height));
+  rectangles.push(new Rectangle(randomInt(1, c.width * 0.9), c.height, 35, 35, "#000000", "#000000", 0));
   rectangles[rectangles.length - 1].draw();
+  rectangles[rectangles.length - 1].setFruit("bomb");
 }
 
-newRects();
-setUpCounter();
-setUpChances();
+setUpAssets();
+ctx.font = "50px Arial";
+ctx.fillStyle = "#000000"
+ctx.textAlign = "center";
+ctx.fillText("starting in:", c.width * 0.5, c.height * 0.5);
+setTimeout(() => {
+  ctx.clearRect(0, 0, c.width, c.height);
+  ctx.fillText("3", c.width * 0.5, c.height * 0.5);
+}, 1000)
+setTimeout(() => {
+  ctx.clearRect(0, 0, c.width, c.height);
+  ctx.fillText("2", c.width * 0.5, c.height * 0.5);
+}, 2000)
+setTimeout(() => {
+  ctx.clearRect(0, 0, c.width, c.height);
+  ctx.fillText("1", c.width * 0.5, c.height * 0.5);
+}, 3000)
+setTimeout(() => {
+  ctx.clearRect(0, 0, c.width, c.height);
+  newRects();
+  setUpCounter();
+  setUpChances();
+  intervalCheckGameOverID = setInterval(checkGameOver, 50);
+  intervalNewRectsID = setInterval(newRects, 4000);
+  intervalCreateBombID = setInterval(createBomb, 6000);
+  intervals = [intervalCheckGameOverID, intervalNewRectsID, intervalCreateBombID];
+}, 4000)
 
-c.addEventListener("mousedown", onMouseDown);
 c.addEventListener("mousemove", onMouseMove);
-
-// to do:
-// make actual physics: done
-// make the fruit not rectangles
-// add background: done
-// add bombs: done
-// add the three chances thing: done
-// figure out delay thing: done
